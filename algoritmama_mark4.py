1d0c21b3598f4176d11e2b683320ca92478057ad
omertarikkrc <102172007+omertarikkrc@users.noreply.github.com>
2022-08-05 00:39:16 +0300

    Add files via upload
    teoride bitti sırada hata bulma ve geliştirme var hadi hayırlıısı

---
+++ algoritmama_mark4.py
@@ -0,0 +1,99 @@
+from dronekit import connect
+from math import cos, pi, sin, tan
+from deneme8 import cx,cy #importing ml's data
+
+# ml data:
+x_axis_ratio =cx/640
+y_axis_ratio =cy/480
+
+
+# Connect to the Vehicle (in this case a UDP endpoint)
+osman = connect("/dev/ttyAMA0", baud=921600, wait_ready=True)
+
+
+
+camera_horizontal_angle = (31.1/180)*pi              # camera horizontal angle
+camera_vertical_angle = (24.4/180)*pi
+height =   osman.location.global_relative_frame.alt                                       # height
+lon_mav =  int(osman.location.global_relative_frame.lon)                         # lon plane come from pixhawk
+lat_mav =  int(osman.location.global_relative_frame.lat)                       # lat plane come from pixhawk
+yaw_mav = osman.heading                                                   # heading cv 
+yaw_mav_rad = (yaw_mav/180)*pi
+ml_ratio = 0.7                                                 # hedef merkeze uzakligi bolu tam ekrann orani + i>
+field_view_vertical = tan(2*camera_vertical_angle)*height      # tanin icini 2x yapma sebebi on arka olmasi 
+field_view_horizantal = tan(2*camera_horizontal_angle)*height  # tanin icini 2x yapma sebebi sağ sol olmasi 
+ground_speed = osman.groundspeed 
+current_horizantal_field_of_view = height*tan(camera_horizontal_angle) #anlik yatay field of view 
+current_vertical_field_of_view = height*tan(camera_vertical_angle)    #anlik dikey field of view
+
+additive_degree_for_frame_horizantal =current_horizantal_field_of_view/0.084    #it varies by 0.00001 degrees at every 0.084 m for istanbul eklenecek coordinate miktari kose coordinate icin
+additive_degree_for_frame_vertical = current_vertical_field_of_view/0.11113295   #it varies by 0.00001 degrees at every 0.11113295 
+# additive_degree_for_frame_horizantal = round(additive_degree_for_frame_horizantal,0)
+# additive_degree_for_frame_vertical= round(additive_degree_for_frame_vertical,0) # hata alirsam duzeltcem
+
+
+
+
+#frame's corner coordinates
+if yaw_mav >0 and yaw_mav <= 90:
+    above_right_point=LocationGlobalRelative(lat_mav +additive_degree_for_frame_horizantal,lon_mav+ additive_degree_for_frame_vertical)
+    above_left_point =LocationGlobalRelative(lat_mav-additive_degree_for_frame_horizantal,lon_mav + additive_degree_for_frame_vertical)
+    bottom_right_point=LocationGlobalRelative(lat_mav+additive_degree_for_frame_horizantal,lon_mav-additive_degree_for_frame_vertical)
+    bottom_left_point =LocationGlobalRelative(lat_mav- additive_degree_for_frame_horizantal,lon_mav- additive_degree_for_frame_vertical)
+    
+
+elif yaw_mav > 90 and yaw_mav <= 180:
+    above_right_point=LocationGlobalRelative(lat_mav -additive_degree_for_frame_horizantal,lon_mav- additive_degree_for_frame_vertical)
+    above_left_point =LocationGlobalRelative(lat_mav-additive_degree_for_frame_horizantal,lon_mav + additive_degree_for_frame_vertical)
+    bottom_right_point=LocationGlobalRelative(lat_mav+additive_degree_for_frame_horizantal,lon_mav-additive_degree_for_frame_vertical)
+    bottom_left_point =LocationGlobalRelative(lat_mav+ additive_degree_for_frame_horizantal,lon_mav+ additive_degree_for_frame_vertical)
+elif yaw_mav > 180 and yaw_mav <= 270:
+    above_right_point=LocationGlobalRelative(lat_mav -additive_degree_for_frame_horizantal,lon_mav- additive_degree_for_frame_vertical)
+    above_left_point =LocationGlobalRelative(lat_mav-additive_degree_for_frame_horizantal,lon_mav + additive_degree_for_frame_vertical)
+    bottom_right_point=LocationGlobalRelative(lat_mav+additive_degree_for_frame_horizantal,lon_mav-additive_degree_for_frame_vertical)
+    bottom_left_point =LocationGlobalRelative(lat_mav+ additive_degree_for_frame_horizantal,lon_mav+ additive_degree_for_frame_vertical)
+else:
+    above_right_point=LocationGlobalRelative(lat_mav -additive_degree_for_frame_horizantal,lon_mav+ additive_degree_for_frame_vertical)
+    above_left_point =LocationGlobalRelative(lat_mav-additive_degree_for_frame_horizantal,lon_mav - additive_degree_for_frame_vertical)
+    bottom_right_point=LocationGlobalRelative(lat_mav-additive_degree_for_frame_horizantal,lon_mav+additive_degree_for_frame_vertical)
+    bottom_left_point =LocationGlobalRelative(lat_mav+ additive_degree_for_frame_horizantal,lon_mav+ additive_degree_for_frame_vertical)
+    
+
+
+
+# according to ml's data target's lat and lon
+target_lat = ((2*additive_degree_for_frame_horizantal)*x_axis_ratio)+ (lat_mav-additive_degree_for_frame_horizantal)
+target_lon = ((2*additive_degree_for_frame_vertical)*y_axis_ratio)+(lon_mav-additive_degree_for_frame_vertical)
+#target coordination
+target_coordination = LocationGlobalRelative(target_lat,target_lon)
+
+
+
+
+#i don't know why i have written this code
+
+lat_speed = ground_speed*sin(yaw_mav_rad)
+lon_speed = ground_speed*cos(yaw_mav_rad)
+
+
+max_observe_view_horizantal_camera_osman = tan(camera_horizontal_angle)*height
+"""above_right_point =LocationGlobalRelative(arp_lat,arp_lon)
+above_left_point =LocationGlobalRelative(alp_lat,alp_lon)
+bottom_right_point=LocationGlobalRelative(brp_lat,brp_lon)
+bottom_left_point=LocationGlobalRelative(blp_lat,blp_lon)"""
+
+
+
+
+"""distance_from_mav = max_observe_view_horizantal_camera_osman * ml_ratio
+
+lat_distance = distance_from_mav*cos(yaw_mav)
+lon_distance = distance_from_mav*sin(yaw_mav)
+
+lon_angle = (360 * lon_distance)/40007.863
+lat_angle = (360 * lat_distance)/31323.0231012
+
+lon_red_point = round(lon_angle + lon_mav,8)
+lat_red_point = round(lat_angle + lat_mav,8)
+
+print(lon_red_point,",",lat_red_point)"""
